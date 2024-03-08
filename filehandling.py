53446fc6e41cdd95a3bb07a53e3354834e72ef83
Saurabh Mistry <72013349+Saurabh-offixial@users.noreply.github.com>
2024-03-08 12:30:51 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -1,13 +1,16 @@
+#File Handling Write Program
 a=open("ABC.txt","w")
-a.write("Saurabh")
-a.write(" Diya")
-a.write(" Meet")
-a.write(" Fenil")
+a.write("Saurabh \n")
+a.write("Diya \n")
+a.write("Meet \n")
+a.write("Fenil")
 
+#File Handling Read Program
 a=open("ABC.txt","r")
 b=a.read()
 print(b)
 
+#File Handling Append Program
 a=open("ABC.txt","a")
 a.write("Hemin")
 a.close()
